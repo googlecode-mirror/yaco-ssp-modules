e639246a1496caca56b5d2deb43138bb89d11b29
Ernesto.Revilla <Ernesto.Revilla@f22ba432-c3e9-11de-9797-47652b86970f>
2010-01-28 13:56:55 +0000

    Faltaba la plantilla de Warning.

---
+++ webct/templates/webct_warning.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Warning template
+ *
+ * Parameters:
+ * - 'warning': Warning.
+ * - 'url': Target URL.
+ *
+ * @package simpleSAMLphp
+ * @version $Id$
+ */
+
+
+$header = $this->t('{webct:webct:warning_header}');
+$generic_text = $this->t('{webct:webct:warning_text}');
+$continue_text = $this->t('{webct:webct:warning_continue}');
+
+$warning = $this->data['warning'];
+$url = $this->data['url'];
+
+$this->includeAtTemplateBase('includes/header.php');
+echo "<h1>$header</h1>\n";
+echo "<p>$generic_text:</p>\n<br />\n";
+echo "<p><b>$warning</b></p>\n<br />\n";
+echo "<p><a href=\"$url\">$continue_text</a></p>\n";
+$this->includeAtTemplateBase('includes/footer.php');
