4e64eead791d2a40fdcf9aff7ca6f5e3b24888e9
pitbulk <pitbulk@gmail.com>
2010-03-19 11:37:14 +0000

    Adding config-template modification to attributevalidator module.

--- attributevalidator/config-template/config-attributevalidator.php
+++ attributevalidator/config-template/config-attributevalidator.php
@@ -1,13 +1,13 @@
 <?php
 
 $config = array (
-   	'auth' => 'saml',
+	'auth' => 'saml',
 	'required_attrs' => array (
 		'givenName', 'cn', 'sn', 'schacSn1', 'schacSn2', 'displayName',
 		'irisMailMainAddress', 'eduPersonScopedAffiliation',
 		'eduPersonAffiliation', 'eduPersonPrimaryAffiliation',
-		'eduPersonPrincipalName', 'eduPersonOrgDN',
-		'schacPersonalUniqueID',
+		'eduPersonPrincipalName',
+		'schacPersonalUniqueID', 'irisMailAlternateAddress',
 	),
 	'recommended_attrs' => array (
 		'irisMailAlternateAddress', 'mail', 'schacUserPresenceID',
@@ -22,15 +22,23 @@ $config = array (
 		'title', 'preferredLanguage', 'schacYearOfBirth',
 		'postalAddress', 'homePostalAddress', 'street', 'l',
 		'postalCode', 'mobile', 'homePhone', 'telephoneNumber',
-		'fax', 'schacCountryOfResidence', 'eduPersonAssurance',
-		'userCertificate', 'userSMIMECertificate',
+		'fax', 'schacCountryOfResidence', 'eduPersonOrgDN',
+		'eduPersonAssurance', 'userCertificate', 'userSMIMECertificate',
 		'irisPublicKey', 'uid', 'o', 'ou', 'labeledURI',
 		'description', 'seeAlso',
 	),
 	'generated_attrs' => array(
-	        'eduPersonTargetedID', 'schacHomeOrganization',
-                'schacHomeOrganizationType',
+		'eduPersonTargetedID', 'schacHomeOrganization',
+		'schacHomeOrganizationType',
 	),
+    'format_validation_regex' => array(
+        'irisMailMainAddress' => '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/',
+        'eduPersonScopedAffiliation' => '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/',
+        'schacPersonalUniqueID' => '/urn:mace:terena.org:schac:personalUniqueID:es:(.+):(.+)/',
+        'schacUserStatus' => '/urn:mace:terena.org:schac:userStatus:es:(.+):([0-9]{8}):(.+):(.+):(.+)/',
+        'irisMailAlternateAddress' => '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/',
+        'mail' => '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/',
+    ),
 );
 
 ?>
